523d426a3b901db4dac64171a953bceb47a0a48d
jcastillo <jorge@latintop.com>
2016-12-13 13:40:43 +0100

    Changes for Kata 1

--- fizz-buzz/KataFizzBuzz.Tests/FizzBuzzTest.cs
+++ fizz-buzz/KataFizzBuzz.Tests/FizzBuzzTest.cs
@@ -1,18 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
-namespace KataFizzBuzz.Tests
-{
-    public class FizzBuzzTest
-    {
+namespace KataFizzBuzz.Tests {
+    public class FizzBuzzTest {
+        private IList<string> ProcessNumbersFrom1To100() {
+            var fizzBuzz = new FizzBuzz(1, 100);
+            return fizzBuzz.ProcessNumbers();
+        }
+
         [Fact]
-        public void my_first_test()
-        {
-            Assert.True(true);
+        public void my_first_test() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("1", result[0]);
+        }
+
+        [Fact]
+        public void FizzBuzz_2_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("2", result[1]);
+        }
+
+        [Fact]
+        public void FizzBuzz_3_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("Fizz", result[2]);
+        }
+
+        [Fact]
+        public void FizzBuzz_5_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("Buzz", result[4]);
+        }
+
+        [Fact]
+        public void FizzBuzz_6_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("Fizz", result[5]);
+        }
+
+        [Fact]
+        public void FizzBuzz_10_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("Buzz", result[9]);
+        }
+
+        [Fact]
+        public void FizzBuzz_15_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("FizzBuzz", result[14]);
+        }
+
+        [Fact]
+        public void FizzBuzz_outOfRange_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => result[100]);
+        }
+
+        [Fact]
+        public void FizzBuzz_13_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("Fizz", result[12]);
+        }
+
+        [Fact]
+        public void FizzBuzz_30_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("FizzBuzz", result[29]);
+        }
+
+        [Fact]
+        public void FizzBuzz_31_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("Fizz", result[30]);
+        }
+
+        [Fact]
+        public void FizzBuzz_33_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("Fizz", result[32]);
+        }
+
+        [Fact]
+        public void FizzBuzz_35_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("FizzBuzz", result[34]);
+        }
+
+        [Fact]
+        public void FizzBuzz_51_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("FizzBuzz", result[50]);
+        }
+
+        [Fact]
+        public void FizzBuzz_52_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("Buzz", result[51]);
+        }
+
+        [Fact]
+        public void FizzBuzz_53_ok() {
+            var result = ProcessNumbersFrom1To100();
+            Assert.Equal("FizzBuzz", result[52]);
         }
     }
 }
--- fizz-buzz/KataFizzBuzz/FizzBuzz.cs
+++ fizz-buzz/KataFizzBuzz/FizzBuzz.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace KataFizzBuzz
-{
-    public class FizzBuzz
-    {
+namespace KataFizzBuzz {
+    public class FizzBuzz {
+        private readonly int _initial;
+        private readonly int _final;
+
+        public FizzBuzz(int initial, int final) {
+            _initial = initial;
+            _final = final;
+        }
+
+        private static string Process(int i) {
+            if (IsFizzBuzz(i))
+                return "FizzBuzz";
+            if (IsFizz(i))
+                return "Fizz";
+            if (IsBuzz(i))
+                return "Buzz";
+            else
+                return i.ToString();
+        }
+
+        private static bool IsFizzBuzz(int i) {
+            return IsMultipleOf3(i) && (IsMultipleOf5(i) || (Has5(i)))
+                   || (Has3(i) && Has5(i));
+        }
+
+        private static bool IsBuzz(int i) {
+            return Has5(i) || IsMultipleOf5(i);
+        }
+
+        private static bool IsFizz(int i) {
+            return Has3(i) || IsMultipleOf3(i);
+        }
+
+        private static bool Has5(int number) {
+            return number.ToString().Contains("5");
+        }
+
+        private static bool Has3(int number) {
+            return number.ToString().Contains("3");
+        }
+
+        public List<string> ProcessNumbers() {
+            var values = new List<string>();
+            for (var i = _initial; i <= _final; i++)
+                values.Add(Process(i));
+            return values;
+        }
+
+        private static bool IsMultipleOf3(int i) {
+            return i % 3 == 0;
+        }
+
+        private static bool IsMultipleOf5(int i) {
+            return i % 5 == 0;
+        }
     }
 }
